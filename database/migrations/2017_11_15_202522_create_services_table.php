0e943a18acaaa5086d72d7110b48fa86b07585ae
HoangNNHH <hoangninh30496@gmail.com>
2017-11-17 20:18:31 +0700

    Fix datatypes

--- database/migrations/2017_11_15_202522_create_services_table.php
+++ database/migrations/2017_11_15_202522_create_services_table.php
@@ -16,7 +16,7 @@ class CreateServicesTable extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('price');
+            $table->integer('price');
             $table->text('description')->nullable();
             $table->timestamps();
         });
