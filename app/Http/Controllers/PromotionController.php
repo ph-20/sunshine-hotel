d7ecb506160906c00344cf500d8298469167c15b
nguyentranthinh <31733726+nguyentranthinh@users.noreply.github.com>
2017-11-23 16:29:29 +0700

    fix PromotionController

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CheckCreatPromotionRequest;
-use App\Http\Requests\CheckEditPromotionRequest;
 use App\Promotion;
 use Toastr;
 
