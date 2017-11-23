cac4c01d81bebbc1cb13817ebd4c201d2764fcbf
nguyentranthinh <31733726+nguyentranthinh@users.noreply.github.com>
2017-11-23 15:32:55 +0700

    fix config/app.php

--- config/app.php
+++ config/app.php
@@ -230,8 +230,6 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Toastr' => Kamaln7\Toastr\Facades\Toastr::class,
-
     ],
-
 ];
 
