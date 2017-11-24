ed4b091b7293ac8bb0d51b755e3b31ad3001bf06
nguyentranthinh <31733726+nguyentranthinh@users.noreply.github.com>
2017-11-24 13:56:53 +0700

    fix

--- public/Lotus/send_mail_contact.php
+++ public/Lotus/send_mail_contact.php
@@ -6,13 +6,13 @@ $subject = $_POST['subject'];
 $msg = $_POST['message'];
 
 
-$headers = 'From: '.$from . "\r\n" .
-    'Reply-To: '.$from . "\r\n" .
+$headers = 'From: ' . $from . "\r\n" .
+    'Reply-To: ' . $from . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-if(mail($to, $subject, $msg, $headers)){
+if (mail($to, $subject, $msg, $headers)) {
     echo '<p class="reservation-confirm">Thanks for your contact. We will reply soon.</p>';
-}else{
+} else {
     echo '<h2>Send email fail</h2>';
 }
 ?>
