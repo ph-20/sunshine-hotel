b01adf62723aca64bdacde3db0320d041d15bf6a
nguyentranthinh <31733726+nguyentranthinh@users.noreply.github.com>
2017-11-24 13:52:56 +0700

    create Font End

---
+++ resources/views/hotel/layouts/checkavailability.blade.php
@@ -0,0 +1,34 @@
+<section class="section-check-availability">
+    <div class="container">
+        <div class="check-availability">
+            <div class="row">
+                <div class="col-lg-3">
+                    <h2>CHECK AVAILABILITY</h2>
+                </div>
+                <div class="col-lg-9">
+                    <form id="ajax-form-search-room" action="search_step_2.php" method="post">
+                        <div class="availability-form">
+                            <input type="text" name="arrive" class="awe-calendar from" placeholder="Arrival Date">
+                            <input type="text" name="departure" class="awe-calendar to" placeholder="Departure Date">
+
+                            <select class="awe-select" name="adults">
+                                <option>Adults</option>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                            </select>
+                            <select class="awe-select" name="children">
+                                <option>Vip</option>
+                                <option>Deluxe</option>
+                                <option>Family</option>
+                            </select>
+                            <div class="vailability-submit">
+                                <button class="awe-btn awe-btn-13">FIND THE BEST RATE</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
